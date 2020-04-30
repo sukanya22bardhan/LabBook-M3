d0187a3f0a8be763959913e684c2aa29204ad5d7
sukanya22bardhan <61135069+sukanya22bardhan@users.noreply.github.com>
2020-04-30 09:18:56 +0530

    Add files via upload

---
+++ Lab4/src/main/java/com/cg/iter/exceptions/ApplicationException.java
@@ -0,0 +1,8 @@
+package com.cg.iter.exceptions;
+
+public class ApplicationException extends RuntimeException {
+
+    public ApplicationException(String message) {
+        super(message);
+    }
+}
